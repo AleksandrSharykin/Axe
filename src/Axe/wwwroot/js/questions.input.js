f448282410fdef03dd590cd03bf954683ae19df0
AleksandrSharykin <30412176+AleksandrSharykin@users.noreply.github.com>
2017-08-16 15:50:23 +0300

    added script to calculate question point value

---
+++ src/Axe/wwwroot/js/questions.input.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    var summator = function () {
+        var score = 0;
+        var txt = $('.answer-score');
+        for (var i = 0; i < txt.length; i++) {
+            score += parseInt($(txt[i]).val());
+        }
+
+        if (!score) {
+            score = 0;
+            $('#totalScore').addClass('text-danger');
+        }
+        else {
+            $('#totalScore').removeClass('text-danger');
+        }
+
+        $('#totalScore').text(score);
+    };
+
+    $('.answer-score').change(summator);
+    summator();
+})
